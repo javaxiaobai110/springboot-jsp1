f45c936cad7a072d1a9d97bb82b19c8a93fa7af9
gitxiaobai <krisandpeters@qq.com>
2018-12-14 17:45:55 +0800

    我是在idea中第二次提交的，已经在该项目中添加了jsp支持，但没有添加jstl等其他相关的jar

--- src/main/java/com/baizhi/springbootjsp1/controller/MyController.java
+++ src/main/java/com/baizhi/springbootjsp1/controller/MyController.java
@@ -1,19 +1,17 @@
 package com.baizhi.springbootjsp1.controller;
 
 
-import com.baizhi.springbootjsp1.domain.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
-@RestController
+@Controller
 public class MyController {
 
     @RequestMapping("/heh")
-    public User hehe(){
+    public String hehe(){
 
-        return new User(1L,"张三",2,"男",new Date());
+        //return new User(1L,"张三",2,"男",new Date());
+        return "index";
     }
 
 }
